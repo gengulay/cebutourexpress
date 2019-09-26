0cf4163e8f02f2dec4a342a7aa5438e9ca800891
Gen Gulay <gen.gulay@easesolutions.com>
2019-09-26 16:54:09 +0800

    first commit

---
+++ public/precache-manifest.2163b783fae6e13119c3068baa21706f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "15a4be7a288dc178facd672977243491",
+    "url": "/img/Bohol-Day-Tour-for-guests-staying-in-Cebu-1_3f7e70564f44c7bf9cecae1214ad30a8.15a4be7a.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "dcec096a2b664e945e8227b68da11bca",
+    "url": "/img/Island-Hopping-Tour_50a2590ab60b273b48d80d46cdc1242f.dcec096a.jpg"
+  },
+  {
+    "revision": "4ee0bb364cb7ca8f0e80",
+    "url": "/js/app.22205fe5.js"
+  },
+  {
+    "revision": "25948b814698c30a114f",
+    "url": "/js/chunk-vendors.575b82b4.js"
+  },
+  {
+    "revision": "f95f638fff9d07180745",
+    "url": "/js/about.3c4193d7.js"
+  },
+  {
+    "revision": "7648bd312b31b03312ac6b381a27edb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b93d980fafbe4dfaa316ccb51444c5a2",
+    "url": "/img/Panglao-Island-Tour_5e5e5311f1d3d0ff65f06f1da8f65c72.b93d980f.jpg"
+  },
+  {
+    "revision": "f23fc3245190c0487a720a45c5b0df09",
+    "url": "/img/HRI-Bohol-Cebu-Tours-logo-1_optimized-1.f23fc324.png"
+  },
+  {
+    "revision": "5b93e3ea151f266fc9e9c3f8acb8f4c6",
+    "url": "/img/Bohol-Westside-Tour_73aa5284eb22d4dc60a0539adee39b4e.5b93e3ea.jpg"
+  },
+  {
+    "revision": "b7e92afccd42e497c2aed85ba6f78097",
+    "url": "/img/Bohol-Tours-most-affordable.b7e92afc.jpg"
+  },
+  {
+    "revision": "fa86d24ebc3dfb29c8b11f7ef87ef10d",
+    "url": "/img/Bohol-Countryside-Tour_ffb7abb3bb822639098add6baca7fc69.fa86d24e.jpg"
+  },
+  {
+    "revision": "25948b814698c30a114f",
+    "url": "/css/chunk-vendors.b89365ac.css"
+  },
+  {
+    "revision": "4ee0bb364cb7ca8f0e80",
+    "url": "/css/app.6e940fad.css"
+  }
+];
